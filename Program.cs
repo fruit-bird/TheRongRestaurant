d113d8a9b7b7ad48857261628de610f493601ee0
fruit-bird <github.ru3w9@slmail.me>
2024-01-25 13:18:40 +0100

    bla

--- Program.cs
+++ Program.cs
@@ -2,16 +2,46 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TheRongRestaurant.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+// async void createRolesandUsers()
+// {
+//     var options = new DbContextOptions<ApplicationDbContext>();
+//     var context = new ApplicationDbContext(options);
+//     var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context), null, null, null, null);
+//     var UserManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(context), null, null, null, null, null, null, null, null);
+//     if (!await roleManager.RoleExistsAsync("Administrator"))
+//     {
+//         var role = new IdentityRole("Administrator");
+//         await roleManager.CreateAsync(role);
+//         var user = new IdentityUser("system");
+//         var chkUser = await UserManager.CreateAsync(user, "Qwerty_12345");
+//         if (chkUser.Succeeded)
+//         {
+//             var result1 = UserManager.AddToRoleAsync(user, "Administroator");
+//         }
+//     }
+//     if (!await roleManager.RoleExistsAsync("Manager"))
+//     {
+//         var role = new IdentityRole("Manager");
+//         await roleManager.CreateAsync(role);
+//     }
+//     if (!await roleManager.RoleExistsAsync("Employee"))
+//     {
+//         var role = new IdentityRole("Employee");
+//         await roleManager.CreateAsync(role);
+//     }
+// }
+// createRolesandUsers();
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+    // .AddRoles<IdentityRole>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
